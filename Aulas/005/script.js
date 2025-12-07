d8c7ec8f1d0df09c1e5a98c5e035d37551720ce2
Gilberto <gilbertolumelofilho@gmail.com>
2025-12-07 00:30:32 -0400

    inicio DOM

---
+++ Aulas/005/script.js
@@ -0,0 +1,4 @@
+document.title = "DOM - alterado"; // document => referenciase ao documento como um todo, descendo o nivel de ramo
+document.body.style.backgroundColor = "gray"; // desce o nivel de referencia, o backgrand esta dentro de style que esta dentro de body que esta no documento
+document.write("Escrevendo...") //função write escreve
+document.write("<h1>Escrevendo titulos...</h1>"); //função write aceita tags
