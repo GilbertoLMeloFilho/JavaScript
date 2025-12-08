ce1b9fbbb93423eef3e42471cc9c467bfd153fc1
Gilberto <gilbertolumelofilho@gmail.com>
2025-12-08 00:00:17 -0400

    day 08/12

--- Aulas/005/script.js
+++ Aulas/005/script.js
@@ -4,6 +4,8 @@ setTimeout(function name(params) { // função setTimeout seta um tempo para alg
     document.getElementById("PrimTit").innerHTML = "Mais um titulo novo" //innerHTML é usado para escrever algo ou reescrever com formatação (CSS)  | innertext tbm escreve porem sem formatacao
 }, 3000); //o tempo esta em milisegundos | 1 segunto = 1000 milisegundos
 
+
+//Funções de seleção de elemento
 //getElementById
 document.getElementById("test").innerHTML = "Selecionado por getElementById" //getElemnetbyId pega o elemento da pagina por id
 
@@ -25,12 +27,27 @@ document.querySelector("div").innerHTML = "Selecionado por querySelector"
 document.querySelectorAll(".segundo")[1].innerHTML = "Selecionado por querySelectorAll" //seletor geral por tag class ou id porem com vetor para escolher( [0] )
 
 
-//Tags de escrita:
+//Função de valores
+let nome = ""
+function armazenarNome (){
+    nome = document.getElementById("nome").value //a função value pega o valor de dentro de um input HTML
+    event.preventDefault() //nao deixa a pagina recaregar ao dar submit
+}
+function MostarNome(){
+    document.getElementById("caixa").innerText = nome
+    event.preventDefault()
+}
+
+//Funções de escrita:
+//innerText
+document.getElementById("innertext").innerText = "<i>innerText</i>" //reescre e le texto porem nao pega texto oculto por css e nao coloca tags html é o mais lento
+
+//innerHTML
+document.getElementById("innerHTML").innerHTML = "<i>innerHTML</i>" //reescre e le texto pega texto oculto por css e é o unico que aceita tags HTML
+
+//textContent
+document.getElementById("textContent").textContent = "<i>textContent</i>" //reescre e le texto pega texto oculto por css e nao coloca tags html, é o mais rapido
+
 //Write
 document.write("document.write") //função write escreve na tela de forma simples sem lugar expecifico e sem tag no HTML, a escrita vai para o final da pagina
 document.write("<h1>document.write</h1>"); //função write aceita tags
-
-//innerText
-document.getElementById("innertext").innerText = "<i>innerText</i>" //reescre e le texto porem nao pega texto oculto por css e nao coloca tags html é o mais lento
-document.getElementById("innerHTML").innerHTML = "<i>innerHTML</i>" //reescre e le texto pega texto oculto por css e é o unico que aceita tags HTML
-document.getElementById("textContent").textContent = "<i>textContent</i>" //reescre e le texto pega texto oculto por css e nao coloca tags html, é o mais rapido
